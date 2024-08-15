4105264de0944b7ed764e10577f3810908a3db81
ri OH <leesulgi66@naver.com>
2024-08-15 17:29:46 +0900

    App.js reset

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,26 +1,5 @@
-import { useEffect, useState } from "react";
-
-function Hello() {
-  function byFn() {
-    console.log("Bye :(")
-  } 
-  function hiFn() {
-    console.log("Hi :)")
-    return byFn;
-  } 
-  useEffect(hiFn, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = ()=>{setShowing((pre)=>!pre)};
-  return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
-    </div>
-  );
+  return <div></div>
 }
 
 export default App;
