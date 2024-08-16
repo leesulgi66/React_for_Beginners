14c09598cc1b0d97ed1a03c4032a55c39843bddf
ri OH <leesulgi66@naver.com>
2024-08-16 19:20:16 +0900

    Coin Tracker

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,5 +1,23 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then(response => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false)});
+  }, []);
+  return (
+  <div>
+    <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+    {loading ? <string>Loading...</string> : 
+    <select>
+    {coins.map((coin) => <option key={coin.id}>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>)}
+    </select>}
+  </div>);
 }
 
 export default App;
