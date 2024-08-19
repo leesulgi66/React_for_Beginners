545743a226b4d9648b1ed4d2204c67ecde98706c
ri OH <leesulgi66@naver.com>
2024-08-19 17:17:00 +0900

    React Router

--- react-for-beginners/src/components/Movie.js
+++ react-for-beginners/src/components/Movie.js
@@ -1,13 +1,23 @@
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+
 function Movie({coverImg, title, summary, genres}) {
     return (
         <div>
             <img src={coverImg} art={title} />
-            <h2>{title}</h2>
+            <h2><Link to="/movie">{title}</Link></h2>
             <p>{summary}</p>
             <ul>
             {genres.map(g => <li key={g}>{g}</li>)}
             </ul>
         </div>);
+}
+
+Movie.prototype = {
+    coverImg : PropTypes.string.isRequired, 
+    title : PropTypes.string.isRequired,
+    summary : PropTypes.string.isRequired,
+    genres : PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default Movie;
