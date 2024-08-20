fee5a7a2c45c6d0956f4e31beab4f3cd2cf3261e
ri OH <leesulgi66@naver.com>
2024-08-20 20:52:17 +0900

    Publishing

--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import PropTypes from "prop-types";
 
 function Detail() {
     const {id} = useParams();
